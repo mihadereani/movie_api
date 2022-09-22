5cee12069c8e61e65bffc6794c368e0d9611cfea
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-09-22 13:32:48 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require("http"),
+  fs = require("fs"),
+  url = require("url");
+
+http
+  .createServer((request, response) => {
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filepath = "";
+
+    fs.appendfile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + newDate() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log");
+        }
+      }
+    );
+
+    if (q.pathname.includes("documentation")) {
+      filepath = __dirname + "/documentation.html";
+    } else {
+      filepath = "index.html";
+    }
+
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+    });
+
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write(data);
+    response.end("Hello Node!\n");
+  })
+  .listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
