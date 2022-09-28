b96d2873454bdf6379cc71277ca89ed6e5a5fe76
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-09-28 22:13:23 +0200

    refactor your code ...

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path');
+
 const app = express();
-const http = require('http');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a',
+});
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Welcome to my book club!\n');
-  })
-  .listen(8080);
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.static('public'));
 
-console.log('My first Node test server is running on Port 8080.');
+
+app.get('/movies', (req, res) => {
+  res.json(toptenmovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
