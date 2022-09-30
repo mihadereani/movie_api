507349b0a8825b0baa4f3ba7c2bc9281aa23f84a
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-09-30 17:09:05 +0200

    define topTenMovies

--- index.js
+++ index.js
@@ -5,8 +5,41 @@ const app = express();
 app.use(morgan('combined'));
 app.use(express.static('public'));
 
+const topTenMovies = [
+  {
+    title: 'The Shawshank Redemption',
+  },
+  {
+    title: 'The Godfather',
+  },
+  {
+    title: 'The Dark Knight',
+  },
+  {
+    title: 'The Godfather Part II',
+  },
+  {
+    title: '12 Angry Men',
+  },
+  {
+    title: "Schindler's List",
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+  },
+  {
+    title: 'Pulp Fiction',
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+  },
+  {
+    title: 'Il buono, il brutto, il cattivo',
+  },
+];
+
 app.get('/movies', (req, res) => {
-  res.json(toptenmovies);
+  res.json(topTenMovies);
 });
 
 app.get('/', (req, res) => {
