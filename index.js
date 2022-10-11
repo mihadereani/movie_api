96b5dd78681fefe0a0fca33ba56805ba68df10a8
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-10-11 23:37:26 +0200

    set app.post() for update

--- index.js
+++ index.js
@@ -167,17 +167,22 @@ app.put('/users/:Username', (req, res) => {
   );
 });
 
-app.post('/users/:id/movies/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-  } else {
-    res.status(400).send('no such user');
-  }
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 app.delete('/users/:id/movies/:movieTitle', (req, res) => {
