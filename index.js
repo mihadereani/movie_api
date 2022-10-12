ec83a1af2755e63d1b4941cc584f837243e3e31f
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-10-12 09:25:25 +0200

    set app.get()

--- index.js
+++ index.js
@@ -19,81 +19,6 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/* let users = [
-  {
-    id: 1,
-    name: 'Kim',
-    favoriteMovies: [],
-  },
-  {
-    id: 2,
-    name: 'Joe',
-    favoriteMovies: ['The Fountain'],
-  },
-];
-
-let movies = [
-  {
-    Title: 'Iron Man',
-    Year: '2008',
-    Released: '02 May 2008',
-    Runtime: '126 min',
-    Genre: 'Action, Adventure, Sci-Fi',
-    Genre: {
-      Name: 'Action',
-      Description: 'Action is where everybody dies',
-    },
-    Director: {
-      Name: 'Jon Favreau',
-      Bio: 'Unknown',
-      Birth: 'Arround 2000',
-    },
-    Actors: 'Robert Downey Jr., Gwyneth Paltrow, Terrence Howard',
-    Plot: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Harry Potter and the Prisoner of Azkaban',
-    Year: '2004',
-    Released: '04 Jun 2004',
-    Runtime: '142 min',
-    Genre: {
-      Name: 'Adventure',
-      Description: 'Adveture takes you to another worlds',
-    },
-    Director: {
-      Name: 'Alfonso Cuarón',
-      Bio: 'Adventorus',
-      Birth: '1984',
-    },
-    Writer: 'J.K. Rowling, Steve Kloves',
-    Actors: 'Daniel Radcliffe, Emma Watson, Rupert Grint',
-    Plot: 'Harry Potter, Ron and Hermione return to Hogwarts School of Witchcraft and Wizardry for their third year of study, where they delve into the mystery surrounding an escaped prisoner who poses a dangerous threat to the young wizard.',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg',
-  },
-  {
-    Title: 'The Devil Wears Prada',
-    Year: '2006',
-    Released: '30 Jun 2006',
-    Runtime: '109 min',
-    Genre: {
-      Name: 'Drama',
-      Description: 'Drama comes with a high in the middle.',
-    },
-    Director: {
-      Name: 'David Frankel',
-      Bio: 'Scary',
-      Birth: '1700',
-    },
-    Writer: 'Aline Brosh McKenna, Lauren Weisberger',
-    Actors: 'Anne Hathaway, Meryl Streep, Adrian Grenier',
-    Plot: 'A smart but sensible new graduate lands a job as an assistant to Miranda Priestly, the demanding editor-in-chief of a high fashion magazine.',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZjQ3ZTIzOTItMGNjNC00MWRmLWJlMGEtMjJmMDM5ZDIzZGM3XkEyXkFqcGdeQXVyMTkzODUwNzk@._V1_SX300.jpg',
-  },
-]; */
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
@@ -106,6 +31,28 @@ app.get('/movies', (req, res) => {
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      req.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      req.status(500).send('Error: ' + err);
     });
 });
 
@@ -129,17 +76,6 @@ app.post('/users', (req, res) => {
             req.status(500).send('Error: ' + error);
           });
       }
-    })
-    .catch((error) => {
-      console.error(error);
-      req.status(500).send('Error: ' + error);
-    });
-});
-
-app.get('/users', (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json.users;
     })
     .catch((error) => {
       console.error(error);
@@ -230,17 +166,6 @@ app.delete('/users/:Username', (req, res) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
-});
-
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('no such movie');
-  }
 });
 
 app.get('/genre/:genreName', (req, res) => {
