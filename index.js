d1355909a254f4dd808c0e4be8ca9eef1bffc689
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-10-29 09:48:02 +0200

    allow all CORS Requests

--- index.js
+++ index.js
@@ -13,21 +13,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 const cors = require('cors');
-const allowedOrigins = ['*', 'http://localhost:1234'];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if ((allowedOrigins, indexOf(origin) === -1)) {
-        const message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 const auth = require('./auth')(app);
 
