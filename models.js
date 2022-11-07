808866a40690a0ed433615ad01886501e544ff0c
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-11-07 12:14:59 +0100

    correct typo

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ const userSchema = mongoose.Schema({
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
 userSchema.statics.hashPassword = (password) => {
